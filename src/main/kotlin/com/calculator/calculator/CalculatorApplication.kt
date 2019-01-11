6ffeccb0ab017d0cb33d135ee4c60b82464c160d
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-01-11 10:42:57 +0100

    set up project

---
+++ src/main/kotlin/com/calculator/calculator/CalculatorApplication.kt
@@ -0,0 +1,12 @@
+package com.calculator.calculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalculatorApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/calculator/calculator/CalculatorApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.calculator.calculator
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
