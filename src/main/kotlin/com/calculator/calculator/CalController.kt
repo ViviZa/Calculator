97012cb93ef1d4cca0d03688ab76d156f489d71a
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-01-30 20:04:39 +0100

    add CalController

---
+++ src/main/kotlin/com/calculator/calculator/CalController.kt
@@ -0,0 +1,14 @@
+package com.calculator.calculator
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+class CalController {
+
+    @GetMapping("/myCalculator")
+    fun getCalculator(): ModelAndView {
+        return ModelAndView()
+    }
+}
