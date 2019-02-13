0ff85f580cbf220910f63d3aa8277f9a2f5564f2
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-02-13 08:47:34 +0100

    add CalControllerTest and CalculatorApplicationTest

---
+++ src/test/kotlin/com/calculator/calculator/CalControllerTest.kt
@@ -0,0 +1,19 @@
+package com.calculator.calculator
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.hamcrest.CoreMatchers.*
+import org.springframework.web.servlet.ModelAndView
+
+class CalControllerTest {
+
+@InjectMocks
+val calController = CalController()
+
+    @Test
+    fun templateIsShown(){
+        val mav : ModelAndView = calController.getCalculator()
+        assertThat(mav.viewName, `is`("calculatorview"))
+    }
+}
--- src/test/kotlin/com/calculator/calculator/CalculatorApplicationTests.kt
+++ src/test/kotlin/com/calculator/calculator/CalculatorApplicationTests.kt
@@ -1,17 +1,33 @@
 package com.calculator.calculator
 
+import io.restassured.RestAssured
+import io.restassured.RestAssured.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.junit.Before
+import org.springframework.boot.web.server.LocalServerPort
+
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CalculatorApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @LocalServerPort
+    var port: Int = 0
+
+    @Before
+    fun setRestAssuredPort() {
+        RestAssured.port = port
+    }
+
+    @Test
+    fun contextLoads() {
+        get("/myCalculator")
+                .then()
+                .assertThat().statusCode(200)
+    }
 
 }
 
