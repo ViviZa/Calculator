8e4890b5915e21637e5c75968dcb91a6441e4fc0
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-02-14 12:09:22 +0100

    just print results of Model-Methods

--- src/main/kotlin/com/calculator/calculator/CalculatorModel.kt
+++ src/main/kotlin/com/calculator/calculator/CalculatorModel.kt
@@ -3,22 +3,18 @@ package com.calculator.calculator
 class CalculatorModel {
 
     fun add(x: Int, y: Int): Int {
-        System.out.println(x+y)
         return x + y
     }
 
     fun subtract(x: Int, y: Int): Int {
-        System.out.println(x-y)
         return x - y
     }
 
     fun multiply(x : Int, y: Int) : Int {
-        System.out.println(x*y)
         return x * y
     }
 
     fun divide(x : Int, y: Int) : Int {
-        System.out.println(x/y)
         return x / y
     }
 }
