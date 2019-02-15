bf0ddfdade3d1c087b84697b2b11ac05d1445082
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-02-15 09:59:07 +0100

    execute the add function on the server side via post endpoint to have less logic in the view

--- src/main/kotlin/com/calculator/calculator/CalController.kt
+++ src/main/kotlin/com/calculator/calculator/CalController.kt
@@ -1,7 +1,9 @@
 package com.calculator.calculator
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
@@ -14,6 +16,14 @@ class CalController {
         val calculatorModel = CalculatorModel();
         val mav = ModelAndView("calculatorview")
         mav.addObject("calculator", calculatorModel)
+        return mav
+    }
+
+    @RequestMapping(value = ["/add"], method = [RequestMethod.POST])
+    fun postCalculator(@ModelAttribute calculator: CalculatorModel) : ModelAndView {
+        val result = calculatorService.add(calculator)
+        val mav = ModelAndView("addedTemplate")
+        mav.addObject("result", result)
         return mav
     }
 }
