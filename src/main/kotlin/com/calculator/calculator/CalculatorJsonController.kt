43118ecf1383d8927f725f988ba10f1fa3c85df3
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-03-12 16:09:27 +0100

    define new controller which returns calculator results as json data 2.0

---
+++ src/main/kotlin/com/calculator/calculator/CalculatorJsonController.kt
@@ -0,0 +1,35 @@
+package com.calculator.calculator
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CalculatorJsonController {
+
+    private val calculatorService = CalculatorService()
+
+    @PostMapping(value = ["/add"])
+    fun add(@RequestBody map : Map<String, Int>): Pair<String, Int> {
+        val result = calculatorService.add(map.getOrDefault("x", 0), map.getOrDefault("y", 0))
+        return Pair("result", result)
+    }
+
+    @PostMapping(value = ["/subtract"])
+    fun subtract(@RequestBody map : Map<String, Int>): Pair<String, Int> {
+        val result = calculatorService.subtract(map.getOrDefault("x", 0), map.getOrDefault("y", 0))
+        return Pair("result", result)
+    }
+
+    @PostMapping(value = ["/multiply"])
+    fun multiply(@RequestBody map : Map<String, Int>): Pair<String, Int> {
+        val result = calculatorService.multiply(map.getOrDefault("x", 0), map.getOrDefault("y", 0))
+        return Pair("result", result)
+    }
+
+    @PostMapping(value = ["/divide"])
+    fun divide(@RequestBody map : Map<String, Int>): Pair<String, Int> {
+        val result = calculatorService.divide(map.getOrDefault("x", 0), map.getOrDefault("y", 0))
+        return Pair("result", result)
+    }
+}
