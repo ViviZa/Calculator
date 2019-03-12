a9294655014b5bbb406378a4bbe90032ff6e25cc
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-03-12 14:51:03 +0100

    method overloading to use CalculatorService in a different context

--- src/main/kotlin/com/calculator/calculator/CalculatorService.kt
+++ src/main/kotlin/com/calculator/calculator/CalculatorService.kt
@@ -6,15 +6,30 @@ class CalculatorService {
         return calculatorModel.x + calculatorModel.y
     }
 
+    fun add(x: Int, y: Int): Int {
+        return x + y
+    }
+
     fun subtract(calculatorModel: CalculatorModel): Int {
         return calculatorModel.x - calculatorModel.y
+    }
+    fun subtract(x: Int, y: Int): Int {
+        return x - y
     }
 
     fun multiply(calculatorModel: CalculatorModel) : Int {
         return calculatorModel.x * calculatorModel.y
     }
 
+    fun multiply(x: Int, y: Int): Int {
+        return x * y
+    }
+
     fun divide(calculatorModel: CalculatorModel) : Int {
         return calculatorModel.x / calculatorModel.y
+    }
+
+    fun divide(x: Int, y: Int): Int {
+        return x / y
     }
 }
