889f59959dd9ef7ccaf06c9e1fc3fa9bb0fc0b28
ViviZa <youwillnevergetthis10>
2019-07-25 17:23:21 +0200

    fix calculation in react view

--- src/main/js/Calculator.js
+++ src/main/js/Calculator.js
@@ -6,13 +6,12 @@ class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            val1 : 3,
-            val2 : 3,
-            result : 0,
-            operation : ''
+            val1 : 0,
+            val2 : 0,
+            result : "",
             };
          this.handleInputChange = this.handleInputChange.bind(this);
-         this.calculate = this.calculate.bind(this);
+         this.handleClick = this.handleClick.bind(this);
     }
 
     handleInputChange(event) {
@@ -23,34 +22,38 @@ class Calculator extends React.Component {
         this.setState({
           [name]: value
         });
-  }
+    }
 
-    calculate(event) {
-        var current = parseInt(this.state.val1) + parseInt(this.state.val2);
+    handleClick(operation) {
+        var current = 0;
+        if(operation == "+") { current = parseInt(this.state.val1) + parseInt(this.state.val2); }
+        if(operation == "-") { current = parseInt(this.state.val1) - parseInt(this.state.val2); }
+        if(operation == "*") { current = parseInt(this.state.val1) * parseInt(this.state.val2); }
+        if(operation == "/") { current = parseInt(this.state.val1) / parseInt(this.state.val2); }
+
         this.setState({
-                  result: current
-                });
-        event.preventDefault();
+           result: current
+        });
     }
 
     render() {
         return (
-            <div>
-                <h4>Enter the values and choose the wanted calculation</h4>
-                 <form onSubmit={this.calculate}>
-                        <label>
-                          Numbers:
-                          <input name="val1" type="number" value={this.state.val1} onChange={this.handleInputChange}/>
-                          <input name="val2" type="number" value={this.state.val2} onChange={this.handleInputChange}/>
-                        </label>
-                        <br/>
-                        <input type="submit" value="+" />
-                 </form>
-                 <br/>
+            <div className="calculator-view">
+                <h4 className="headline">Enter the values and choose the wanted calculation</h4>
+                Numbers:
+                <form>
+                    <input name="val1" type="number" value={this.state.val1} onChange={this.handleInputChange}/>
+                    <input name="val2" type="number" value={this.state.val2} onChange={this.handleInputChange}/>
+                </form>
+                <br/>
+                <button onClick={() => this.handleClick("+")}>+</button>
+                <button onClick={() => this.handleClick("-")}>-</button>
+                <button onClick={() => this.handleClick("*")}>*</button>
+                <button onClick={() => this.handleClick(":")}>:</button>
+                <br/>
                 <div className="result">{this.state.result}</div>
             </div>
         )
     }
-
 }
 export default Calculator;
